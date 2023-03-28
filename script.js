e7d92ba0bc70fcf41c4642742b38791d59ee7944
AdirCesario <adircesario@outlook.com>
2023-03-28 10:49:47 -0300

    desafio

---
+++ script.js
@@ -0,0 +1,29 @@
+let  numberOne= prompt ("Escolha o Primeiro Número")
+let  numberTwo= prompt ("Escolha o Segundo Número")
+
+let v1 = Number(numberOne)
+let v2 = Number(numberTwo)
+
+alert (`A Soma de ${v1}+${v2} = ${v1 + v2} `)
+
+if ((v1+v2) % 2 ==0) {
+alert("A soma dos dois números é par")
+}
+else {
+    alert("A Soma dos dois números é impar")
+}
+if (v1 === v2) {
+    alert("Os dos dois números são iguais")
+    }
+    else {
+        alert("Os dois números são diferentes")
+    }
+
+
+alert (`A Subtração de ${v1}-${v2} = ${v1 - v2} `)
+
+alert (`A Multiplicação ${v1}*${v2} = ${v1 * v2} `)
+
+alert (`A Soma de ${v1}/${v2} = ${v1 / v2} `)
+
+
